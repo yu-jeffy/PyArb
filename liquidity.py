11d4983f69942f9786118e4f3a91013908c88b72
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-11-29 22:29:42 +0000

    liquidity non-existant pools fix

--- liquidity.py
+++ liquidity.py
@@ -2,6 +2,8 @@ from web3 import Web3
 import json
 from dotenv import load_dotenv
 import os
+from pools import pool_addresses  # Import pool_addresses from pools.py
+from web3.exceptions import BadFunctionCallOutput
 
 # Load environment variables from .env file
 load_dotenv()
@@ -9,8 +11,9 @@ load_dotenv()
 # Set up your Web3 provider using the environment variable
 web3 = Web3(Web3.HTTPProvider(os.getenv('WEB3_PROVIDER_URL')))
 
-# Should return True if connected
-print(web3.is_connected())  
+# Check if connected to Web3
+if web3.is_connected():
+    print("We are connected to Web3!")
 
 # Load the Uniswap V3 Pool contract ABI from the external JSON file
 with open('uniswap_pool_abi.json', 'r') as abi_file:
@@ -18,12 +21,39 @@ with open('uniswap_pool_abi.json', 'r') as abi_file:
 
 # Function to query liquidity from a pool contract
 def get_pool_liquidity(pool_address):
-    checksum_address = Web3.to_checksum_address(pool_address)  # Convert to checksum address
-    pool_contract = web3.eth.contract(address=checksum_address, abi=pool_abi)
-    return pool_contract.functions.liquidity().call()
+    try:
+        checksum_address = Web3.to_checksum_address(pool_address)  # Convert to checksum address
+        pool_contract = web3.eth.contract(address=checksum_address, abi=pool_abi)
+        return pool_contract.functions.liquidity().call()
+    except BadFunctionCallOutput:
+        # If the contract call fails, return 0 liquidity
+        return 0
 
-# Example usage:
-# Suppose you have a pool address calculated from the previous steps
-example_pool_address = '0xa6Cc3C2531FdaA6Ae1A3CA84c2855806728693e8'
-liquidity = get_pool_liquidity(example_pool_address)
-print(f'Liquidity in the pool: {liquidity}')
+# Function to get the liquidity for each pool and return an array of pool information with liquidity
+def get_pool_liquidities(pool_addresses):
+    pool_liquidities = []
+    for item in pool_addresses:
+        ticker_0, address_0 = item[0]
+        ticker_1, address_1 = item[1]
+        fee = item[2]
+        pool_address = item[3]
+        
+        # Query the liquidity
+        liquidity = get_pool_liquidity(pool_address)
+        
+        # Append the liquidity to the tuple
+        item_with_liquidity = item + (liquidity,)
+        
+        # Append the result to the pool_liquidities array
+        pool_liquidities.append(item_with_liquidity)
+    return pool_liquidities
+
+# Get the array of pool information with liquidity
+pool_liquidities = get_pool_liquidities(pool_addresses)
+
+# Filter out pools with zero liquidity and create a new list
+pool_liquidities_valid = [pool_info for pool_info in pool_liquidities if pool_info[4] > 0]
+
+# Print the array of pool information with non-zero liquidity
+for pool_info in pool_liquidities_valid:
+    print(f'Pair: {pool_info[0][0]}-{pool_info[1][0]} ({pool_info[0][1]}, {pool_info[1][1]}), Fee: {pool_info[2]}, Pool Address: {pool_info[3]}, Liquidity: {pool_info[4]}')

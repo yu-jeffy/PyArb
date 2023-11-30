7eeb7f00a851ef1cfa78016c6279ef8cdebb4874
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-11-30 04:08:47 +0000

    decimal flip

--- arbitrage.py
+++ arbitrage.py
@@ -30,7 +30,7 @@ def calculate_price_with_decimals(sqrt_price_x96, token0_decimals, token1_decima
     # Convert sqrtPriceX96 to actual price
     price = (sqrt_price_x96 / (1 << 96)) ** 2
     # Adjust for token decimals
-    price_adjusted = price * (10 ** token0_decimals) / (10 ** token1_decimals)
+    price_adjusted = price * (10 ** token1_decimals) / (10 ** token0_decimals)
     return price_adjusted
 
 # get how many decimals a coin has for accurate price calculation

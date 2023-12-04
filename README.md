03c091fc95e03b9f7d71a819c574a6b8b4f984a6
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-12-04 02:39:32 +0000

    arbitrage simple v2

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # PyArb
-web3.py arbitrage on uniswap
+
+## Overview
+This project aims to develop a Python-based arbitrage bot for Uniswap V3. The bot is designed to identify and potentially exploit price discrepancies across different Uniswap V3 pools for the same token pair. The project includes scripts for fetching pool data, calculating prices with proper token decimals, and identifying simple and triangular arbitrage opportunities.
+
+## Project Structure
+The project is divided into several Python files, each serving a specific purpose:
+
+- `coins.py`: Contains a dictionary mapping token tickers to their respective Ethereum contract addresses.
+- `decimals.py`: Fetches and stores the number of decimals for each token using the Web3 library and the ERC-20 `decimals` ABI.
+- `pricing.py`: Queries Uniswap V3 pool contracts for liquidity and other pool-specific data.
+- `arbitrage_simple.py`: Identifies simple arbitrage opportunities by comparing the prices of a token across different pools.
+- `arbitrage_triangle.py`: Identifies triangular arbitrage opportunities by finding cycles of length three in a graph representation of token pools.
+
+## Key Concepts
+
+- **Pool Data**: Information about each pool, including token addresses, liquidity, `sqrt_price_x96`, and pool fees.
+- **Price Calculation**: Conversion of `sqrt_price_x96` to actual token prices, adjusted for token decimals.
+- **Graph Construction**: Representation of token pools as a graph where nodes are tokens and edges are pools with associated prices.
+- **Arbitrage Identification**: Algorithms to detect price differences that can be exploited for profit, considering the direction of trades.
+
+## Usage
+
+1. **Environment Setup**: Install the required Python packages, such as `web3`, by running `pip install -r requirements.txt`.
+
+2. **Ethereum Node Configuration**: Create a `.env` file in the root directory of the project with your Ethereum node HTTP provider URL. For example:
+   ```
+   WEB3_PROVIDER_URL="https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"
+   ```
+
+3. **Token Configuration**: In `coins.py`, add the tokens you want the bot to monitor by specifying their ticker symbols and Ethereum contract addresses. For example:
+   ```python
+   # coins.py
+   coins = {
+       "WETH": '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+       "WBTC": '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+       "DAI": '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+       # Add more tokens as needed
+   }
+   ```
+
+4. **Fetch Token Decimals**: Run `decimals.py` to fetch and store the number of decimals for each token in the `coins_decimals` dictionary. This information is crucial for accurate price calculations.
+
+5. **Query Pool Data**: Use `pricing.py` to query Uniswap V3 pools for the latest data, including liquidity, prices, and fees.
+
+6. **Identify Arbitrage Opportunities**: Execute `arbitrage_simple.py` to identify simple arbitrage opportunities across different pools for the same token pair. Alternatively, run `arbitrage_triangle.py` to identify triangular arbitrage opportunities by finding profitable cycles in the token graph.
+
+7. **Review Results**: Analyze the output for potential arbitrage opportunities. The scripts will print information about profitable trades, including the tokens involved, the pools to execute the trades, and the expected profit margins.
+
+## Considerations
+
+- **Slippage**: The bot currently does not account for slippage, which can significantly impact the profitability of arbitrage trades, especially in pools with low liquidity.
+- **Transaction Costs**: Ethereum gas fees are not considered in the current implementation and must be factored into the profitability calculation.
+- **Execution Time**: Prices on decentralized exchanges can change rapidly, so the bot must execute trades quickly to capture the identified opportunities.
+- **Smart Contract Interaction**: A production version of the bot would require smart contract integration to automate trade execution.
+
+## Future Work
+
+- Implement slippage estimation and include it in the profitability calculation.
+- Integrate with Ethereum wallets and smart contracts to enable automated trade execution.
+- Add real-time monitoring and alerting for arbitrage opportunities.
+- Optimize the bot's performance and improve its resilience to changing market conditions.
+
+## Disclaimer
+
+This project is for educational purposes only. Arbitrage trading carries financial risks, and you should perform your own due diligence before running any trading bot on a live network. The developers of this project are not responsible for any financial losses incurred from using this bot.
--- arbitrage_simple.py
+++ arbitrage_simple.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 from pricing import pool_data
 from decimals import coins_decimals
+from coins import coins
 
 # Load environment variables from .env file
 load_dotenv()
@@ -25,37 +26,108 @@ def calculate_price_with_decimals(sqrt_price_x96, token0_address, token1_address
     price_adjusted = price * (10 ** token0_decimals) / (10 ** token1_decimals)
     return price_adjusted
 
-def find_simple_arbitrage_opportunities(pool_data):
+def find_simple_arbitrage_opportunities_high_low(pool_data):
+    # Dictionary to store prices for each token pair
     prices = {}
+    # Dictionary to store pool addresses for each token pair
+    pool_addresses = {}
+
+    # Iterate over the pool data to populate the prices and pool_addresses dictionaries
+    for pool in pool_data:
+        token0, token1, _, pool_address, _, sqrt_price_x96, token0_address, token1_address, _, _ = pool
+        # Calculate the price of token1 in terms of token0
+        price_token0_to_token1 = calculate_price_with_decimals(sqrt_price_x96, token0_address, token1_address)
+        # Calculate the price of token0 in terms of token1 (inverse price)
+        price_token1_to_token0 = 1 / price_token0_to_token1
+
+        # Store the prices and pool addresses using a consistent token pair ordering
+        token_pair = tuple(sorted([token0[0], token1[0]]))
+
+        if token_pair not in prices:
+            prices[token_pair] = {'token0_to_token1': [], 'token1_to_token0': []}
+            pool_addresses[token_pair] = {'token0_to_token1': [], 'token1_to_token0': []}
+
+        # Store the prices and corresponding pool addresses
+        prices[token_pair]['token0_to_token1'].append(price_token0_to_token1)
+        prices[token_pair]['token1_to_token0'].append(price_token1_to_token0)
+        pool_addresses[token_pair]['token0_to_token1'].append(pool_address)
+        pool_addresses[token_pair]['token1_to_token0'].append(pool_address)
+
+    # Compare prices for simple arbitrage opportunities
+    for token_pair, price_dict in prices.items():
+        # Check if there are prices in both directions
+        if price_dict['token0_to_token1'] and price_dict['token1_to_token0']:
+            # Find the best (lowest) buy price for token0_to_token1 and the best (highest) sell price for token1_to_token0
+            best_buy_price = min(price_dict['token0_to_token1'])
+            best_sell_price = max(price_dict['token1_to_token0'])
+            best_buy_pool = pool_addresses[token_pair]['token0_to_token1'][price_dict['token0_to_token1'].index(best_buy_price)]
+            best_sell_pool = pool_addresses[token_pair]['token1_to_token0'][price_dict['token1_to_token0'].index(best_sell_price)]
+
+            # Calculate the price difference and percentage difference
+            price_difference = best_sell_price - best_buy_price
+            percentage_difference = (price_difference / best_buy_price) * 100
+
+            # Check if there is an arbitrage opportunity
+            if price_difference > 0:
+                print(f"Arbitrage opportunity for {token_pair}: "
+                      f"Buy at {best_buy_pool} (Price: {best_buy_price}), "
+                      f"Sell at {best_sell_pool} (Price: {best_sell_price}). "
+                      f"Price difference: {price_difference} ({percentage_difference:.2f}%)")
+            else:
+                print(f"No arbitrage opportunity for {token_pair}.")
+
+
+def find_simple_arbitrage_opportunities(pool_data):
+    # Dictionary to store prices for each direction of each token pair
+    prices = {}
+    # Dictionary to store pool addresses for each direction of each token pair
     pool_addresses = {}
 
     for pool in pool_data:
+        # Unpack the pool data
         token0, token1, _, pool_address, _, sqrt_price_x96, token0_address, token1_address, _, _ = pool
-        # Use the updated price calculation function with decimals
-        price = calculate_price_with_decimals(sqrt_price_x96, token0_address, token1_address)
-        token_pair = (token0[0], token1[0])
         
-        if token_pair not in prices:
-            prices[token_pair] = []
-            pool_addresses[token_pair] = []
-        prices[token_pair].append(price)
-        pool_addresses[token_pair].append(pool_address)
-    
-    # Compare prices for simple arbitrage
+        # Calculate the price of token1 in terms of token0
+        price_token0_to_token1 = calculate_price_with_decimals(sqrt_price_x96, token0_address, token1_address)
+        # Calculate the price of token0 in terms of token1 (inverse price)
+        price_token1_to_token0 = 1 / price_token0_to_token1
+
+        # Store the prices and pool addresses for both directions
+        token_pair_0_to_1 = (token0[0], token1[0])
+        token_pair_1_to_0 = (token1[0], token0[0])
+
+        if token_pair_0_to_1 not in prices:
+            prices[token_pair_0_to_1] = []
+            pool_addresses[token_pair_0_to_1] = []
+        if token_pair_1_to_0 not in prices:
+            prices[token_pair_1_to_0] = []
+            pool_addresses[token_pair_1_to_0] = []
+
+        prices[token_pair_0_to_1].append(price_token0_to_token1)
+        prices[token_pair_1_to_0].append(price_token1_to_token0)
+        pool_addresses[token_pair_0_to_1].append(pool_address)
+        pool_addresses[token_pair_1_to_0].append(pool_address)
+
+    # Compare prices for simple arbitrage opportunities
     for token_pair, price_list in prices.items():
-        if len(price_list) > 1 and max(price_list) != min(price_list):
-            max_price = max(price_list)
-            min_price = min(price_list)
-            price_difference = max_price - min_price
-            percentage_difference = (price_difference / min_price) * 100
+        # Check if the reverse token pair also exists in the dictionary
+        reverse_token_pair = (token_pair[1], token_pair[0])
+        if reverse_token_pair in prices:
+            # Find the best buy price for token_pair and the best sell price for reverse_token_pair
+            best_buy_price = min(price_list)
+            best_sell_price = max(prices[reverse_token_pair])
+            best_buy_pool = pool_addresses[token_pair][price_list.index(best_buy_price)]
+            best_sell_pool = pool_addresses[reverse_token_pair][prices[reverse_token_pair].index(best_sell_price)]
 
-            max_price_pool = pool_addresses[token_pair][price_list.index(max_price)]
-            min_price_pool = pool_addresses[token_pair][price_list.index(min_price)]
+            # Calculate the price difference and percentage difference
+            price_difference = best_sell_price - best_buy_price
+            percentage_difference = (price_difference / best_buy_price) * 100
 
-            print(f"Arbitrage opportunity for {token_pair}: "
-                  f"Buy at {min_price_pool} (Price: {min_price}), "
-                  f"Sell at {max_price_pool} (Price: {max_price}). "
-                  f"Price difference: {price_difference} ({percentage_difference:.2f}%)")
+            # Check if there is an arbitrage opportunity
+            if price_difference > 0:
+                print(f"Arbitrage opportunity for {token_pair}: "
+                      f"Buy at {best_buy_pool} (Price: {best_buy_price}), "
+                      f"Sell at {best_sell_pool} (Price: {best_sell_price}). "
+                      f"Price difference: {price_difference} ({percentage_difference:.2f}%)")
 
 find_simple_arbitrage_opportunities(pool_data)
-
